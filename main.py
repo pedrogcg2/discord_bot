a926895572a7aa9e9cc2bc3234cd3fe4038903f7
pedrogcg2 <pedrogcgds@gmail.com>
2023-01-12 13:35:47 -0300

    substindo o event loop run por start

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from music import MusicCog
 import asyncio
 from help import HelpCog
 
+
 intents = discord.Intents.default()
 intents.message_content = True
+
 
 bot = commands.Bot(command_prefix='pg!', intents=intents)
 
@@ -13,10 +15,20 @@ bot = commands.Bot(command_prefix='pg!', intents=intents)
 async def setup():
     await bot.add_cog(MusicCog(bot))
     await bot.add_cog(HelpCog(bot))
-
+    
 
 async def main():
     await setup()
+    await bot.start(TOKEN)
+
+@bot.event
+async def on_ready():
+    print("logged in sucessfully")
+
+@bot.event
+async def on_disconnect():
+    print("disconnected sucessfully")
+
 
 file = open("token.txt", mode="r")
 TOKEN = file.readline()
@@ -24,4 +36,3 @@ file.close()
 
 
 asyncio.run(main())
-bot.run(TOKEN)
